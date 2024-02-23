1f90f6267a2d0149b0667ab281a3324672451685
Nahtan <nathan.titirangi@gmail.com>
2024-02-24 10:03:43 +1300

    the big refactor

--- files/scripts/default_tab.lua
+++ files/scripts/default_tab.lua
@@ -5,7 +5,7 @@ local tab = {
 
 	end,
 	get_elems = function(lib, window, tab_idx)
-		return { lib:create_text_gui_elem("No tab.", 1, "centre", "centre", true, 0, 0) }
+		return { lib:create_text_gui_elem("", 1, "centre", "centre", true, 0, 0), lib:create_text_gui_elem("Yes Hello.",2, "low", "low", false, 0, 0) }
 	end
 }
 
